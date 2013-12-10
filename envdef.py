8f6c2fc5b5e5802c55528b1dfe564c157e3eb648
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2013-12-10 12:46:46 +0900

    improve envdef.py example

--- envdef.py
+++ envdef.py
@@ -1,13 +1,34 @@
+# ----------------------------- #
+#  Test Environment Definition  #
+# ----------------------------- #
+
+###
+### Jubatus Configuration
+###
 env.prefix('/opt/jubatus')
 #env.prefix('/tmp/jubatus')
 #env.variable('LD_LIBRARY_PATH', '/tmp/jubatus/lib:/opt/jubatus/lib')
-#env.workdir('/tmp/jubatest-work')
-env.cluster_prefix('sample-')
 
-env.zookeeper('127.0.0.1', 2181)
-
+###
+### Test Node Configuration
+###
 env.node('127.0.0.1', range(19199,19299))
 
-env.param('JUBATUS_TUTORIAL_DIR', '/home/kenichi/Development/jubatus-tutorial-python')
-env.param('JUBATUS_BENCH_CLASSIFIER', '/home/kenichi/Development/jubatus-benchmark/jubatus-bench-classifier')
-env.param('JUBATUS_BENCH_CLASSIFIER_DATASET', '/home/kenichi/Development/jubatus-benchmark/url_svmlight/Day0.svm')
+###
+### ZooKeeper Configuration
+###
+env.zookeeper('127.0.0.1', 2181)
+
+###
+### Miscellaneous Node Configuration
+###
+env.workdir('/tmp')
+env.cluster_prefix('sample-')
+
+###
+### Test Parameters
+###
+basedir = '/home/kenichi/Development'
+env.param('JUBATUS_TUTORIAL_DIR', basedir + '/jubatus-tutorial-python')
+env.param('JUBATUS_BENCH_CLASSIFIER', basedir + '/jubatus-benchmark/jubatus-bench-classifier')
+env.param('JUBATUS_BENCH_CLASSIFIER_DATASET', basedir + '/jubatus-benchmark/url_svmlight/Day0.svm')
