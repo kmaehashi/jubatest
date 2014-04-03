f4c04488c76a97ed3fb888a01bd4cfb76eac184f
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-04-03 18:29:55 +0900

    fix use case test to run with config variation

--- test/usecase/api_normal.py
+++ test/usecase/api_normal.py
@@ -4,9 +4,7 @@ from jubatest import *
 from jubatest.log import LogLevel
 
 class TestBase(object):
-    def test(self):
-        self._start()
-
+    def do_test(self):
         cli = self.target.get_client(self.name)
 
         # query server and check result
@@ -19,31 +17,43 @@ class TestBase(object):
         logs = self.target.log().level(LogLevel.INFO).message('start listening at port').get()
         self.assertEqual(1, len(logs))
 
-class StandaloneUseCase(JubaTestCase, TestBase):
     @classmethod
     def setUpCluster(cls, env):
-        cls.node0 = env.get_node(0)
-        cls.server0 = env.server_standalone(cls.node0, CLASSIFIER, default_config(CLASSIFIER))
-        cls.target = cls.server0
-        cls.name = ''
+        cls.env = env
 
-    def _start(self):
+    @classmethod
+    def generateTests(cls, env):
+        for (key,config) in get_configs(CLASSIFIER).items():
+            yield cls.config_test, key, config
+
+    def config_test(self, key, config):
+        self._start(config)
+        try:
+            self.do_test()
+        except:
+            self._stop()
+            raise
+
+class StandaloneUseCase(JubaTestCase, TestBase):
+    def _start(self, config):
+        self.name = ''
+        self.node0 = self.env.get_node(0)
+        self.server0 = self.env.server_standalone(self.node0, CLASSIFIER, config)
+        self.target = self.server0
         self.server0.start()
 
     def _stop(self):
         self.server0.stop()
 
 class DistributedUseCase(JubaTestCase, TestBase):
-    @classmethod
-    def setUpCluster(cls, env):
-        cls.node0 = env.get_node(0)
-        cls.cluster = env.cluster(CLASSIFIER, default_config(CLASSIFIER))
-        cls.server0 = env.server(cls.node0, cls.cluster)
-        cls.keeper0 = env.keeper(cls.node0, CLASSIFIER)
-        cls.target = cls.keeper0
-        cls.name = cls.cluster.name
+    def _start(self, config):
+        self.node0 = self.env.get_node(0)
+        self.cluster = self.env.cluster(CLASSIFIER, config)
+        self.server0 = self.env.server(self.node0, self.cluster)
+        self.keeper0 = self.env.keeper(self.node0, CLASSIFIER)
+        self.target = self.keeper0
+        self.name = self.cluster.name
 
-    def _start(self):
         self.server0.start()
         self.keeper0.start()
 
