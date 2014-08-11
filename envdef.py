748ea19750f7405af8d9676568dead74c8eb7cdb
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-08-11 15:54:25 +0900

    improve envdef.py example

--- envdef.py
+++ envdef.py
@@ -30,4 +30,8 @@ env.cluster_prefix('sample')
 ###
 ### Test Parameters
 ###
-env.include('envdef_local.py')
+#env.include('envdef_local.py')
+basedir = '/home/jubatus/Development'
+env.param('JUBATUS_TUTORIAL_DIR', basedir + '/jubatus-tutorial-python')
+env.param('JUBATUS_BENCH_CLASSIFIER', basedir + '/jubatus-benchmark/jubatus-bench-classifier')
+env.param('JUBATUS_BENCH_CLASSIFIER_DATASET', basedir + '/jubatus-benchmark/url_svmlight/Day0.svm')
